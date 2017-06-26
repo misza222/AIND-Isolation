0d92357ca62ef18bae0ddd50d4cdeff06a78430f
Michal Pawlowski <misza222@gmail.com>
2017-06-26 18:41:07 +0200

    Add custom heuristics functions to judge the state of the board

--- agent_test.py
+++ agent_test.py
@@ -5,8 +5,9 @@ cases used by the project assistant are not public.
 
 import unittest
 
-import isolation
-import game_agent
+from isolation import Board
+from sample_players import *
+from game_agent import *
 
 from importlib import reload
 
@@ -15,13 +16,19 @@ class IsolationTest(unittest.TestCase):
     """Unit tests for isolation agents"""
 
     def setUp(self):
-        reload(game_agent)
-        self.player1 = "Player1"
-        self.player2 = "Player2"
-        self.game = isolation.Board(self.player1, self.player2)
+        pass
 
-    def test_something(self):
-        self.assertTrue(self.game == self.game)
+    def test_minimax_plays_the_game(self):
+        player1 = MinimaxPlayer(score_fn=improved_score, timeout=1.0, search_depth=1)
+        player2 = GreedyPlayer()
+        game = Board(player1, player2)
+        game.play()
+
+    def test_minimax_timesout(self):
+        player1 = MinimaxPlayer(score_fn=improved_score, timeout=0, search_depth=1)
+        player2 = GreedyPlayer()
+        game = Board(player1, player2)
+        game.play()
 
 if __name__ == '__main__':
     unittest.main()
